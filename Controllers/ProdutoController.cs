bcd6445a740ad31dece39252ad78fc12f480d5a4
Ariel Vieira <ariel.vieira@yandex.com>
2026-08-04 13:48:51 -0300

    Requisicao POST (nivel basico)

--- Controllers/ProdutoController.cs
+++ Controllers/ProdutoController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using EstoqueApi.Models;
 
 namespace EstoqueApi.Controllers;
 
@@ -7,18 +9,51 @@ namespace EstoqueApi.Controllers;
 [Route("api/produtos")]     // define a rota padrao:  http://localhost:xxxx/api/produtos
 public class ProdutoController : ControllerBase
 {
+        private static List<Produto> produtos = new()
+    {
+        new Produto
+        {
+            Id = 1,
+            Nome = "Mouse Gamer",
+            Preco = 149.90m
+        },
+
+        new Produto
+        {
+            Id = 2,
+            Nome = "Teclado Mecânico",
+            Preco = 289.90m
+        }
+    };
 
 
     [HttpGet]               // Diz: Quando Alguém executar um GET na rota padrao, execute este método
-    public string Teste()
+    public Produto Teste()
     {
-        return "Minha primeira API REST EM ASP.NET!";
+        return new Produto
+        {
+            Id = 1,
+            Nome = "Produto Teste",
+            Preco = 99.99m
+        };
     }
 
-    [HttpGet("ping")]      // Adiciona um novo caminho a rota padrão: http://localhost:xxxx/api/produtos/ping
-    public string Ping()
+
+
+    [HttpGet("lista")]
+    public List<Produto> GetProdutos()
     {
-        return "pong";
+        return produtos;
     }
+
+
+
+    [HttpPost]
+    public string AdicionarProduto([FromBody] Produto item) // [FromBody] diz: O objeto virá no corpo da requisição
+    {
+        produtos.Add(item);
+        return "Produto adicionado com sucesso";
+    }
+
 
 }
--- Models/Produto.cs
+++ Models/Produto.cs
@@ -11,7 +11,7 @@ public class Produto
 {
     public int Id { get; set; }
     public string Nome { get; set; } = string.Empty;
-    public int Quantidade { get; set; }
+    //public int Quantidade { get; set; }
     public decimal Preco { get; set; }
 
 }
